fa7fbc2d46f6fcd55ed9d51fdafad59254d6729a
Evan Garrett <evang522@gmail.com>
2017-12-14 16:12:01 -0500

    create media queries for CSS breakpoints on needs calculator

---
+++ public/js/needcalc.js
@@ -0,0 +1,11 @@
+'use strict';
+/*Function of this script:
+
+To calculate how many rendered cups of each food are necessary based on a given variable of how many people are being supported,
+what kinds of meals people eat for breakfast, lunch, and dinner, and what percentage of the time each meal is eaten. 
+
+Accordingly, we will be separating the calculations into different meals and providing a list of the different types of food available,
+then allowing for five different types of food combinations to be specified for each meal. Then we will allow the user to specify the percentage that each meal
+will be eaten.
+*/
+
