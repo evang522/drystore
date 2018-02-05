566ea865a6b4142abf4d4ddb57cf9e2ad01f2346
Evan Garrett <evang522@gmail.com>
2018-02-04 23:23:29 -0500

    Included log viewing API and route, log view page and link in nav menu

--- routes/logs.js
+++ routes/logs.js
@@ -3,11 +3,37 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
+const path = require('path');
+
+
+router.get('/view', (req,res) => {
+  if (req.isAuthenticated()) {
+    res.sendFile(path.join(__dirname, '../html/', 'logs.html'));
+  } else {
+    res.render('notauthenticated');
+  }
+});
+
+
+router.get('/delete', (req,res) => {
+  if (req.isAuthenticated()) {
+    const deleteLogs = fs.readFileSync('./logs/deletelogs.txt','utf8');
+    res.send(JSON.stringify(deleteLogs));
+  } else {
+    res.render('notauthenticated');
+  }
+});
+
 
 router.get('/add', (req,res) => {
-  const addLogs = fs.readFile('../logs/addlogs.txt', 'utf8');
-  console.log(addLogs);
+  if (req.isAuthenticated()) {
+    const addLogs = fs.readFileSync('./logs/addlogs.txt', 'utf8');
+    res.send(JSON.stringify(addLogs));
+  } else {
+    res.render('notauthenticated');
+  }
 });
+
 
 
 module.exports = router;
